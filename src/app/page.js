4e38568392989564dac8bd6c30dbbf9a730733cb
seongju76 <sungju7694@naver.com>
2024-11-15 17:52:35 +0900

    11/15

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
 import Image from 'next/image';
 
+// 함수 선언식 (기명 함수)
+function func1() {
+    <div>함수 선언식</div>;
+}
+
+// 함수 표현식 (익명 함수)
+const func2 = function () {
+    <div>함수 선언식</div>;
+};
+
+// 화살표 함수
+const Func3 = () => {
+    <div>함수 선언식</div>;
+};
+
 export default function Home() {
     console.log(true ? '참' : '거짓');
 
@@ -13,10 +28,44 @@ export default function Home() {
         height: 180,
     };
 
-    const array = [1, 2, 3];
+    const array = ['menu1', 'menu2', 'menu3'];
+
+    const users = [
+        { name: '겨울', age: 2 },
+        { name: '가을', age: 3 },
+        { name: '여름', age: 1 },
+    ];
+
+    const info = {
+        name: '홍길동',
+        phone: 'ㅇㅇㅇ', // null은 값이 없음을 의미
+    };
 
     return (
         <div>
+            {/* or 연산자는 처음 만나는 참값을 출력 */}
+            {info.name}의 전화번호는 {info.phone || '폰번호 없음'}입니다.
+            {/* 컴포넌트 호출 */}
+            {/* <Func1 />
+            <Func2 />
+            <Func3 /> */}
+            <ul>
+                {users.map((item, index) => (
+                    // const item = users[0];
+                    // const item = users[1];
+                    // const item = users[2];
+
+                    <li key={index}>
+                        {item.name}이는 {item.age}살 입니다.
+                    </li>
+                ))}
+            </ul>
+            <ul>
+                {/* map() 메서드를 사용해서 반복 */}
+                {array.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
             {array[0]}
             {array[1]}
             {array[2]} <br />
